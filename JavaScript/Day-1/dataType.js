79db40aaf3d8ae0b8572398049ab09f73e51dd2b
MUBASSHIRALI0710 <mubasshirali0710@gmail.com>
2026-04-16 15:07:27 +0530

    Java Script Primitive Data Type

---
+++ JavaScript/Day-1/dataType.js
@@ -0,0 +1,37 @@
+// Number	----> A number representing a numeric value
+let length = 5;
+let wight = 3.5;
+
+
+// Bigint ------>	A number representing a large integer
+let l=16564513548799562455665n;
+console.log(l);
+let y = BigInt(1234567890123456789012345)
+console.log(y);
+
+
+// String ----->	A text of characters enclosed in quotes
+let str = "ramu";
+console.log(str);
+let name = 'shyam';
+console.log(name);
+
+
+// Boolean ---->	A data type representing true or false
+let student = false;
+console.log(student)
+let teacher = true;
+console.log(teacher);
+
+
+// Undefined ---->	A variable with no assigned value
+let rollNo ;
+console.log(rollNo);
+console.log(typeof rollNo) // to check data type of variable usinge typeof variable_Name
+
+
+// Null ----->	A value representing object absence
+let balance = null;
+console.log(balance);
+console.log(typeof balance)
+// output is object but this ERROR by js Creater .... typeof null returns "object": This is the result of the historical bug
---
+++ JavaScript/Day-1/operators.js
@@ -0,0 +1,25 @@
+
+let a = 15;
+let b = 5;
+let sum = a + b; // Addition operators
+console.log(sum);
+
+let sub = a - b; // Subtraction operators
+console.log(sub)
+
+let mul = a * b; // Multiplication
+console.log(mul)
+
+let div = a / b; // division
+console.log(div)
+
+let mod = a % b; // Modulus to find remender
+console.log(mod)
+
+let increment = 5;
+increment++; // increment = 5+1 = 6 
+console.log(increment)
+
+let decrement = 5;
+decrement--; // decrement = 5-1 = 4
+console.log(decrement)
